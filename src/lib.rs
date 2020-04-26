50668701295fc035ef0b509014678c6e1926cfd0
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2020-04-26 16:37:22 +0200

    Add rustfmt.toml.

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,19 @@
 
 use regex::RegexBuilder;
 use std::fmt;
-use std::fs::{File, read_dir};
+use std::fs::{read_dir, File};
 use std::io::Read;
 use std::path::{Path, PathBuf};
 use std::str::FromStr;
-use toml::Value;
 use toml::de::Error as TomlError;
+use toml::Value;
 
-const MANIFEST_NAME: &str   = "Cargo.toml";
-const MARKER_START: &str    = "<!-- cargo-sync-readme start -->";
-const MARKER_END: &str      = "<!-- cargo-sync-readme end -->";
-const MARKER_RE: &str       = "^<!-- cargo-sync-readme -->\r?$";
+const MANIFEST_NAME: &str = "Cargo.toml";
+const MARKER_START: &str = "<!-- cargo-sync-readme start -->";
+const MARKER_END: &str = "<!-- cargo-sync-readme end -->";
+const MARKER_RE: &str = "^<!-- cargo-sync-readme -->\r?$";
 const MARKER_START_RE: &str = "^<!-- cargo-sync-readme start -->\r?$";
-const MARKER_END_RE: &str   = "^<!-- cargo-sync-readme end -->\r?$";
+const MARKER_END_RE: &str = "^<!-- cargo-sync-readme end -->\r?$";
 const RUST_LANG_ANNOTATION: &str = "rust";
 
 /// Common Markdown code-block state.
@@ -25,23 +25,24 @@ const RUST_LANG_ANNOTATION: &str = "rust";
 enum CodeBlockState {
   None,
   InWithBackticks,
-  InWithTildes
+  InWithTildes,
 }
 
 #[derive(Debug)]
 pub enum FindManifestError {
   CannotFindManifest,
   CannotOpenManifest(PathBuf),
-  TomlError(TomlError)
+  TomlError(TomlError),
 }
 
 impl fmt::Display for FindManifestError {
   fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
     match *self {
       FindManifestError::CannotFindManifest => f.write_str("Cannot find manifest (Cargo.toml)."),
-      FindManifestError::CannotOpenManifest(ref path) =>
-        write!(f, "Cannot open manifest at path {}.", path.display()),
-      FindManifestError::TomlError(ref e) => write!(f, "TOML error: {}.", e)
+      FindManifestError::CannotOpenManifest(ref path) => {
+        write!(f, "Cannot open manifest at path {}.", path.display())
+      }
+      FindManifestError::TomlError(ref e) => write!(f, "TOML error: {}.", e),
     }
   }
 }
@@ -49,29 +50,33 @@ impl fmt::Display for FindManifestError {
 #[derive(Debug)]
 pub struct Manifest {
   pub toml: Value,
-  pub parent_dir: PathBuf
+  pub parent_dir: PathBuf,
 }
 
 impl Manifest {
   fn new(toml: Value, path: PathBuf) -> Self {
-    Manifest { toml, parent_dir: path.parent().unwrap().to_owned() }
+    Manifest {
+      toml,
+      parent_dir: path.parent().unwrap().to_owned(),
+    }
   }
 
   /// Get the TOML-formatted manifest by looking up the current directory; if not found, go to the
   /// parent directory and recursively retry until one is found… eventually.
-  pub fn find_manifest<P>(dir: P) -> Result<Self, FindManifestError> where P: AsRef<Path> {
+  pub fn find_manifest<P>(dir: P) -> Result<Self, FindManifestError>
+  where
+    P: AsRef<Path>,
+  {
     let dir = dir.as_ref();
 
     if let Ok(mut dir_entry) = read_dir(dir) {
-      if let Some(file_entry) = dir_entry.find(
-        |entry| {
-          match entry {
-            Ok(entry) if entry.file_name() == MANIFEST_NAME => true,
-            _ => false
-          }
-        }) {
+      if let Some(file_entry) = dir_entry.find(|entry| match entry {
+        Ok(entry) if entry.file_name() == MANIFEST_NAME => true,
+        _ => false,
+      }) {
         let path = file_entry.unwrap().path();
-        let mut file = File::open(&path).map_err(|_| FindManifestError::CannotOpenManifest(path.clone()))?;
+        let mut file =
+          File::open(&path).map_err(|_| FindManifestError::CannotOpenManifest(path.clone()))?;
         let mut file_str = String::new();
 
         let _ = file.read_to_string(&mut file_str);
@@ -104,12 +109,12 @@ impl Manifest {
           (true, true) => match prefer_doc_from {
             Some(PreferDocFrom::Binary) => Some(main_path),
             Some(PreferDocFrom::Library) => Some(lib_path),
-            _ => None
-          }
+            _ => None,
+          },
 
           (true, _) => Some(lib_path),
           (_, true) => Some(main_path),
-          _ => None
+          _ => None,
         }
       }
     }
@@ -130,14 +135,15 @@ impl Manifest {
   fn entry_point_from_toml(&self, prefer_from: Option<PreferDocFrom>) -> Option<String> {
     let lib = self.toml.get("lib");
     let bin = self.toml.get("bin");
-    let preference =
-      match prefer_from {
-        Some(PreferDocFrom::Binary) => bin.clone(),
-        Some(PreferDocFrom::Library) => lib.clone(),
-        _ => None
-      };
+    let preference = match prefer_from {
+      Some(PreferDocFrom::Binary) => bin.clone(),
+      Some(PreferDocFrom::Library) => lib.clone(),
+      _ => None,
+    };
 
-    preference.or(lib).or(bin)
+    preference
+      .or(lib)
+      .or(bin)
       .and_then(|v| v.get("path"))
       .and_then(Value::as_str)
       .map(|s| s.to_owned())
@@ -148,7 +154,7 @@ impl Manifest {
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum PreferDocFrom {
   Binary,
-  Library
+  Library,
 }
 
 impl FromStr for PreferDocFrom {
@@ -158,12 +164,15 @@ impl FromStr for PreferDocFrom {
     match s {
       "bin" => Ok(PreferDocFrom::Binary),
       "lib" => Ok(PreferDocFrom::Library),
-      _ => Err("not a valid preference".to_owned())
+      _ => Err("not a valid preference".to_owned()),
     }
   }
 }
 
-fn load_file_to_string<P>(path: P) -> String where P: AsRef<Path> {
+fn load_file_to_string<P>(path: P) -> String
+where
+  P: AsRef<Path>,
+{
   let mut file = File::open(path.as_ref()).unwrap();
   let mut content = String::new();
   let _ = file.read_to_string(&mut content);
@@ -173,7 +182,6 @@ fn load_file_to_string<P>(path: P) -> String where P: AsRef<Path> {
 
 fn transform_inner_doc(doc: &str, show_hidden_doc: bool, crlf: bool) -> String {
   let mut codeblock_st = CodeBlockState::None;
-
 
   let lines: Vec<String> = doc
     .lines()
@@ -198,10 +206,15 @@ fn transform_inner_doc(doc: &str, show_hidden_doc: bool, crlf: bool) -> String {
 
   let sanitized_annotated_lines: Vec<String> = lines
     .into_iter()
-    .map(|line| if crlf && line == "\r\n" || line == "\n" { line } else { line[offset..].to_owned() })
+    .map(|line| {
+      if crlf && line == "\r\n" || line == "\n" {
+        line
+      } else {
+        line[offset..].to_owned()
+      }
+    })
     .map(|line| annotate_code_blocks(&mut codeblock_st, line, crlf))
     .collect();
-
 
   sanitized_annotated_lines
     .into_iter()
@@ -216,29 +229,39 @@ fn transform_inner_doc(doc: &str, show_hidden_doc: bool, crlf: bool) -> String {
 }
 
 /// Open a file and get its main inner documentation (//!), applying filters if needed.
-pub fn extract_inner_doc<P>(path: P, show_hidden_doc: bool, crlf: bool) -> String where P: AsRef<Path> {
+pub fn extract_inner_doc<P>(path: P, show_hidden_doc: bool, crlf: bool) -> String
+where
+  P: AsRef<Path>,
+{
   let doc = load_file_to_string(path);
-  
+
   transform_inner_doc(&doc, show_hidden_doc, crlf)
 }
 
 #[derive(Debug, Eq, PartialEq)]
 pub enum TransformError {
   CannotReadReadme(PathBuf),
-  MissingOrIllFormatMarkers
+  MissingOrIllFormatMarkers,
 }
 
 impl fmt::Display for TransformError {
   fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
     match *self {
-      TransformError::CannotReadReadme(ref path) => write!(f, "Cannot read README at {}.", path.display()),
-      TransformError::MissingOrIllFormatMarkers => f.write_str("Markers not found or ill-formed; check your file again."),
+      TransformError::CannotReadReadme(ref path) => {
+        write!(f, "Cannot read README at {}.", path.display())
+      }
+      TransformError::MissingOrIllFormatMarkers => {
+        f.write_str("Markers not found or ill-formed; check your file again.")
+      }
     }
   }
 }
 
 /// Open and read a README file.
-pub fn read_readme<P>(path: P) -> Result<String, TransformError> where P: AsRef<Path> {
+pub fn read_readme<P>(path: P) -> Result<String, TransformError>
+where
+  P: AsRef<Path>,
+{
   let path = path.as_ref();
   let mut file = File::open(path).map_err(|_| TransformError::CannotReadReadme(path.to_owned()))?;
   let mut content = String::new();
@@ -251,13 +274,11 @@ pub fn read_readme<P>(path: P) -> Result<String, TransformError> where P: AsRef<
 /// Transform a readme file and return its content with the documentation injected, if any.
 ///
 /// Perform any required other transformations if asked by the user.
-pub fn transform_readme<C, R>(
-  content: C,
-  doc: R,
-  crlf: bool
-) -> Result<String, TransformError>
-where C: AsRef<str>,
-      R: AsRef<str> {
+pub fn transform_readme<C, R>(content: C, doc: R, crlf: bool) -> Result<String, TransformError>
+where
+  C: AsRef<str>,
+  R: AsRef<str>,
+{
   let content = content.as_ref();
   let doc = doc.as_ref();
 
@@ -267,8 +288,12 @@ where C: AsRef<str>,
 
   if let Some(marker_match) = marker_re.find(&content) {
     // try to look for the sync marker (first time using the tool)
-    let first_part = &content[0 .. marker_match.start()];
-    let second_part = &content[if crlf { marker_match.end() - 1 } else { marker_match.end() } ..];
+    let first_part = &content[0..marker_match.start()];
+    let second_part = &content[if crlf {
+      marker_match.end() - 1
+    } else {
+      marker_match.end()
+    }..];
 
     Ok(reformat_with_markers(first_part, doc, second_part, crlf))
   } else {
@@ -285,13 +310,17 @@ where C: AsRef<str>,
 
     match (marker_start, marker_end) {
       (Some(start_match), Some(end_match)) => {
-        let first_part = &content[0 .. start_match.start()];
-        let second_part = &content[if crlf { end_match.end() - 1 } else { end_match.end() } ..];
+        let first_part = &content[0..start_match.start()];
+        let second_part = &content[if crlf {
+          end_match.end() - 1
+        } else {
+          end_match.end()
+        }..];
 
         Ok(reformat_with_markers(first_part, doc, second_part, crlf))
-      },
+      }
 
-      _ => Err(TransformError::MissingOrIllFormatMarkers)
+      _ => Err(TransformError::MissingOrIllFormatMarkers),
     }
   }
 }
@@ -301,9 +330,15 @@ where C: AsRef<str>,
 // The crlf` parameter is used to insert a '\r' before '\n'.
 fn reformat_with_markers(first_part: &str, doc: &str, second_part: &str, crlf: bool) -> String {
   if crlf {
-    format!("{}{}\r\n\r\n{}\r\n{}{}", first_part, MARKER_START, doc, MARKER_END, second_part)
+    format!(
+      "{}{}\r\n\r\n{}\r\n{}{}",
+      first_part, MARKER_START, doc, MARKER_END, second_part
+    )
   } else {
-    format!("{}{}\n\n{}\n{}{}", first_part, MARKER_START, doc, MARKER_END, second_part)
+    format!(
+      "{}{}\n\n{}\n{}{}",
+      first_part, MARKER_START, doc, MARKER_END, second_part
+    )
   }
 }
 
@@ -326,19 +361,19 @@ fn annotate_code_blocks(st: &mut CodeBlockState, line: String, crlf: bool) -> St
     }
 
     CodeBlockState::InWithTildes => {
-        if line.starts_with("~~~") {
-          *st = CodeBlockState::None;
-        }
+      if line.starts_with("~~~") {
+        *st = CodeBlockState::None;
+      }
 
-        line
+      line
     }
 
     CodeBlockState::InWithBackticks => {
-        if line.starts_with("```") {
-          *st = CodeBlockState::None;
-        }
+      if line.starts_with("```") {
+        *st = CodeBlockState::None;
+      }
 
-        line
+      line
     }
   }
 }
@@ -431,7 +466,10 @@ mod tests {
     let doc = "//!```\n//!fn add(a: u8, b: u8) -> u8 { a + b }\n//!```";
     let output = transform_inner_doc(doc, false, false);
 
-    assert_eq!(output, "```rust\nfn add(a: u8, b: u8) -> u8 { a + b }\n```\n".to_owned());
+    assert_eq!(
+      output,
+      "```rust\nfn add(a: u8, b: u8) -> u8 { a + b }\n```\n".to_owned()
+    );
   }
 
   #[test]
@@ -439,7 +477,10 @@ mod tests {
     let doc = "//!```\r\n//!fn add(a: u8, b: u8) -> u8 { a + b }\r\n//!```";
     let output = transform_inner_doc(doc, false, true);
 
-    assert_eq!(output, "```rust\r\nfn add(a: u8, b: u8) -> u8 { a + b }\r\n```\r\n".to_owned());
+    assert_eq!(
+      output,
+      "```rust\r\nfn add(a: u8, b: u8) -> u8 { a + b }\r\n```\r\n".to_owned()
+    );
   }
 
   #[test]
@@ -455,6 +496,9 @@ mod tests {
     let doc = "//!```text\r\n//!echo Hello, World!\r\n//!```";
     let output = transform_inner_doc(doc, false, true);
 
-    assert_eq!(output, "```text\r\necho Hello, World!\r\n```\r\n".to_owned());
+    assert_eq!(
+      output,
+      "```text\r\necho Hello, World!\r\n```\r\n".to_owned()
+    );
   }
 }
